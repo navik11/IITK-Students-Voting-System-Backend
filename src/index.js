9162fbef87f43f411d3579e078cffd71dcdbd57a
navik11 <128390875+navik11@users.noreply.github.com>
2024-01-10 09:36:19 +0530

    Moving app to index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,38 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//import routes here
+
+import candidateRouter from "./routes/candidates.route.js";
+import gbmRoute from "./routes/gbm.route.js";
+import adminRoute from "./routes/admin.route.js";
+
+//routes use declaration
+app.get("/", (req, res) => {
+    res.json("Server is live");
+});
+app.use("/api/v1/candidate", candidateRouter);
+app.use("/api/v1/gbm", gbmRoute);
+app.use("/api/v1/admin", adminRoute);
+
 dotenv.config();
 
 connectDB()
