6730091d756ef42780df508ac79c98d748c46e8c
navik11 <128390875+navik11@users.noreply.github.com>
2024-01-10 11:29:40 +0530

    Debugging 8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv";
-import connectDB from "../api/db/index.js";
 import { app }from "./app.js";
 
+import mongoose from "mongoose";
+import { DB_NAME } from "../src/constants.js";
+
 dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_NAME}`
+        );
+        console.log(
+            `Mongodb connected !!\n DB Host ${connectionInstance.connection.host}`
+        );
+    } catch (err) {
+        console.log(`MongoDB connection failed`, err);
+        process.exit(1);
+    }
+};
 
 connectDB()
     .then(() => {
