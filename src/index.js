85e355313f55b092865bb0dbfd4614d702b387a8
navik11 <128390875+navik11@users.noreply.github.com>
2024-01-10 09:50:26 +0530

    Debugging 0

--- src/index.js
+++ src/index.js
@@ -1,63 +1,33 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
+// import connectDB from "./db/index.js";
+// import app from "./app.js";
 
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
-const DB_NAME = "voting_system_database";
+// dotenv.config();
 
-dotenv.config();
+// connectDB()
+//     .then(() => {
+//         app.listen(process.env.PORT || 5667, () => {
+//             console.log(`Sever is live at port: ${process.env.PORT}`);
+//         });
+//     })
+//     .catch((error) => {
+//         console.log(`MongoDB connection failed`);
+//     });
 
-const app = express();
+import express from "express"
 
-app.use(
-    cors({
-        origin: process.env.CORS_ORIGIN,
-        credentials: true,
-    })
-);
+const app = express()
+const PORT = 8000
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
-//import routes here
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
 
-// import candidateRouter from "./routes/candidates.route.js";
-// import gbmRoute from "./routes/gbm.route.js";
-// import adminRoute from "./routes/admin.route.js";
-
-//routes use declaration
-app.get("/", (req, res) => {
-    res.json("Server is live");
-});
-// app.use("/api/v1/candidate", candidateRouter);
-// app.use("/api/v1/gbm", gbmRoute);
-// app.use("/api/v1/admin", adminRoute);
-
-
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(
-            `${process.env.MONGODB_URI}/${DB_NAME}`
-        );
-        console.log(
-            `Mongodb connected !!\n DB Host ${connectionInstance.connection.host}`
-        );
-    } catch (err) {
-        console.log(`MongoDB connection failed`, err);
-        process.exit(1);
-    }
-};
-
-connectDB()
-    .then(() => {
-        app.listen(process.env.PORT || 5667, () => {
-            console.log(`Sever is live at port: ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log(`MongoDB connection failed`);
-    });
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+  })
+  
