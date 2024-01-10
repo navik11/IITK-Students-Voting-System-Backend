09f3a60a25b330fcf49026b603e53d4294159581
navik11 <128390875+navik11@users.noreply.github.com>
2024-01-10 11:48:01 +0530

    Debugging 9

--- src/app.js
+++ src/app.js
@@ -16,16 +16,16 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-// //import routes here
+//import routes here
 
-// import candidateRouter from "./routes/candidates.route.js";
-// import gbmRoute from "./routes/gbm.route.js";
-// import adminRoute from "./routes/admin.route.js";
+import candidateRouter from "./routes/candidates.route.js";
+import gbmRoute from "./routes/gbm.route.js";
+import adminRoute from "./routes/admin.route.js";
 
-// //routes use declaration
-// app.use("/api/v1/candidate", candidateRouter);
-// app.use("/api/v1/gbm", gbmRoute);
-// app.use("/api/v1/admin", adminRoute);
+//routes use declaration
+app.use("/api/v1/candidate", candidateRouter);
+app.use("/api/v1/gbm", gbmRoute);
+app.use("/api/v1/admin", adminRoute);
 
 app.get("/", (req, res) => {
     res.json("Server is live");
