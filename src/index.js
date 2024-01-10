ad2f5ed40546197981165f152a600c265398a96b
navik11 <128390875+navik11@users.noreply.github.com>
2024-01-10 10:09:00 +0530

    Debugging 1

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
-// import dotenv from "dotenv";
-// import connectDB from "./db/index.js";
-// import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
-// dotenv.config();
+dotenv.config();
 
-// connectDB()
-//     .then(() => {
-//         app.listen(process.env.PORT || 5667, () => {
-//             console.log(`Sever is live at port: ${process.env.PORT}`);
-//         });
-//     })
-//     .catch((error) => {
-//         console.log(`MongoDB connection failed`);
-//     });
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 5667, () => {
+            console.log(`Sever is live at port: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(`MongoDB connection failed`);
+    });
 
-import express from "express"
+// import express from "express"
 
-const app = express()
-const PORT = 8000
+// const app = express()
+// const PORT = 8000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
+// app.get('/about', (req, res) => {
+//   res.send('About route 🎉 ')
+// })
 
-app.listen(PORT, () => {
-    console.log(`✅ Server is running on port ${PORT}`);
-  })
+// app.listen(PORT, () => {
+//     console.log(`✅ Server is running on port ${PORT}`);
+//   })
   
